00ae0b17b058e30ba9513af5487be920e7e409a7
PatagoniaFruit <osaez@bestberrychile.cl>
2025-09-26 00:15:02 -0300

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,6 @@
+const productsData = [
+{ id: 'prod-0', name: 'Aceite 500 ML', unit: 'unidad', measureUnit: 'unidades', fobValue: 3.26, fobCurrency: 'EUR', unitsPerCase: 18, casesPerPallet: 95 },
+{ id: 'prod-1', name: 'Aceite 1 L', unit: 'unidad', measureUnit: 'unidades', fobValue: 5.88, fobCurrency: 'EUR', unitsPerCase: 12, casesPerPallet: 77 },
+{ id: 'prod-2', name: 'Pulpa de Mango con Pepa', unit: 'kg', measureUnit: 'kg', fobValue: 3.00, fobCurrency: 'USD', unitsPerCase: null, casesPerPallet: null },
+{ id: 'prod-3', name: 'Pulpa de Mango sin Pepa', unit: 'kg', measureUnit: 'kg', fobValue: 3.10, fobCurrency: 'USD', unitsPerCase: null, casesPerPallet: null }
+];
