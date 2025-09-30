ccee29b3a40f1c72453e6d677b4d385bc64959de
PatagoniaFruit <osaez@bestberrychile.cl>
2025-09-30 10:37:46 -0300

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,6 +1,92 @@
 const productsData = [
-{ id: 'prod-0', name: 'Aceite 500 ML', unit: 'unidad', measureUnit: 'unidades', fobValue: 3.26, fobCurrency: 'EUR', unitsPerCase: 18, casesPerPallet: 95 },
-{ id: 'prod-1', name: 'Aceite 1 L', unit: 'unidad', measureUnit: 'unidades', fobValue: 5.88, fobCurrency: 'EUR', unitsPerCase: 12, casesPerPallet: 77 },
-{ id: 'prod-2', name: 'Pulpa de Mango con Pepa', unit: 'kg', measureUnit: 'kg', fobValue: 3.00, fobCurrency: 'USD', unitsPerCase: null, casesPerPallet: null },
-{ id: 'prod-3', name: 'Pulpa de Mango sin Pepa', unit: 'kg', measureUnit: 'kg', fobValue: 3.10, fobCurrency: 'USD', unitsPerCase: null, casesPerPallet: null }
+    {
+        "id": "prod-0",
+        "name": "Aceite 500 ML",
+        "unit": "unidad",
+        "measureUnit": "unidades",
+        "fobValue": 3.26,
+        "fobCurrency": "EUR",
+        "unitsPerCase": 18,
+        "casesPerPallet": 95
+    },
+    {
+        "id": "prod-1",
+        "name": "Aceite 1 L",
+        "unit": "unidad",
+        "measureUnit": "unidades",
+        "fobValue": 5.88,
+        "fobCurrency": "EUR",
+        "unitsPerCase": 12,
+        "casesPerPallet": 77
+    },
+    {
+        "id": "prod-2",
+        "name": "Pulpa de Mango con Pepa",
+        "unit": "kg",
+        "measureUnit": "kg",
+        "fobValue": 3,
+        "fobCurrency": "USD",
+        "unitsPerCase": null,
+        "casesPerPallet": null
+    },
+    {
+        "id": "prod-3",
+        "name": "Pulpa de Mango sin Pepa",
+        "unit": "kg",
+        "measureUnit": "kg",
+        "fobValue": 3.1,
+        "fobCurrency": "USD",
+        "unitsPerCase": null,
+        "casesPerPallet": null
+    },
+    {
+        "id": "prod-620",
+        "name": "Mango Picado",
+        "unit": "kg",
+        "measureUnit": "kg",
+        "fobValue": 1.73,
+        "fobCurrency": "USD",
+        "unitsPerCase": null,
+        "casesPerPallet": null
+    },
+    {
+        "id": "prod-119",
+        "name": "Pulpa de Mango",
+        "unit": "kg",
+        "measureUnit": "kg",
+        "fobValue": 1.6,
+        "fobCurrency": "USD",
+        "unitsPerCase": null,
+        "casesPerPallet": null
+    },
+    {
+        "id": "prod-986",
+        "name": "Pulpa Chirimoya",
+        "unit": "kg",
+        "measureUnit": "kg",
+        "fobValue": 3.3,
+        "fobCurrency": "USD",
+        "unitsPerCase": null,
+        "casesPerPallet": null
+    },
+    {
+        "id": "prod-446",
+        "name": "Aceite de Oliva Jaen 500 ml",
+        "unit": "unidades",
+        "measureUnit": "unidades",
+        "fobValue": 3.05,
+        "fobCurrency": "EUR",
+        "unitsPerCase": 12,
+        "casesPerPallet": 100
+    },
+    {
+        "id": "prod-136",
+        "name": "Aceite de Oliva Jaen 1 L",
+        "unit": "unidades",
+        "measureUnit": "unidades",
+        "fobValue": 5.1,
+        "fobCurrency": "EUR",
+        "unitsPerCase": 6,
+        "casesPerPallet": 120
+    }
 ];
